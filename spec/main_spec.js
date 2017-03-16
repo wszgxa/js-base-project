31f3f69efebe2e51031ccb1bd0aad5aa2a8fc91a
hiluluke <hiluluke@hotmail.com>
2017-03-16 22:09:26 +0800

    update Analysis word part

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,16 +9,29 @@ chai.use(sinonChai);
 var wordAnalysis = require("../lib/main.js");
 
 
-describe("test wordsAnalysis class", function(){
+describe("test wordsAnalysis class", () => {
 
-    it("should read words.txt give a file", function(){
+    it("should read words.txt give a file", () => {
         // given
         var expect_string = "In the last year, we've been updating pull requests with features that help teams give feedback and make sure only the highest quality code makes it into their projects. You can leave, manage, request, and dismiss reviews, as well as protect branches and limit merging rights. With restricted review dismissals, you can also ensure important feedback gets addressed."
 
         // when
         var result = wordAnalysis.getWords().words;
-        
+
         // then
         expect(result).to.equal(expect_string);
     });
+    it("should return true words count give", () => {
+        // given
+        wordAnalysis.words = "in the last year lalal in hehe year in";
+        
+        // when
+        wordAnalysis.recordWords()
+        
+        // then
+        expect(wordAnalysis.wordsResults).to.have.deep.property('[0].word', 'in');
+        expect(wordAnalysis.wordsResults).to.have.deep.property('[0].num', 3);        
+        expect(wordAnalysis.wordsResults).to.have.deep.property('[1].word', 'year');        
+        expect(wordAnalysis.wordsResults).to.have.deep.property('[1].num', 2);        
+    })
 });
